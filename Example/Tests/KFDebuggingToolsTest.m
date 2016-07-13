2f3d9189a58234a9e526ae0454f7a14e900e3e71
Igor Zubko <igor.zubko@icloud.com>
2016-07-13 14:54:38 +0300

    Added tests for degger checks

---
+++ Example/Tests/KFDebuggingToolsTest.m
@@ -0,0 +1,23 @@
+//
+//  KFDebuggingToolsTest.m
+//  ObjC-KFStdLib
+//
+//  Created by Igor Zubko on 13.07.16.
+//  Copyright © 2016 Igor Zubko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <KFDebuggingTools.h>
+
+@interface KFDebuggingToolsTest : XCTestCase
+@end
+
+@implementation KFDebuggingToolsTest
+
+- (void)test_should_be_in_debugger
+{
+	int result = DDebugIsInDebugger();
+	expect(result).to.equal(1);
+}
+
+@end
